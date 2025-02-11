fdcaddfe9a3e7b1840a1947f13b824bfcc0c4688
tommyglee <leeguhn@grinnell.edu>
2025-02-11 18:21:51 +0900

    renderings

--- script.js
+++ script.js
@@ -38,8 +38,8 @@ const pages = [
     },
     { id: 'digitalmedia', file: 'content/digitalmedia/index.md', title: 'Digital Media',
         projects: [
-            { id: 'narcissus', file: 'content/digitalmedia/narcissus.md', title: 'Narcissus' },
             { id: 'renderings', file: 'content/digitalmedia/renderings.md', title: 'Renderings' },
+            { id: 'narcissus', file: 'content/digitalmedia/narcissus.md', title: 'Narcissus' },
             { id: 'relic', file: 'content/digitalmedia/relic.md', title: 'Relic' },
             { id: 'hansel', file: 'content/digitalmedia/hansel.md', title: 'Hansel' },
         ]
